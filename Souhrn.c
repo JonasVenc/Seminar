37a4ba372b41870fcb5cb48511eb03b4283de779
Jonáš Venc <jonas.venc@sezam.cz>
2022-03-31 19:29:34 +0200

    Doplnění o funkce, pole, práce se soubory a knihovny
    Na písemku
    
    Co-Authored-By: macanek <48349774+macanek@users.noreply.github.com>

--- Souhrn.c
+++ Souhrn.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#include <ctime>
+
 main(){}
 
 //definování proměnných
 int x=1; //celé číslo
 float y=1.1; //desetinné číslo
+
+int a, b; //pro další příklady
+
 
 //vypsání
 printf("");
@@ -65,3 +72,39 @@ printf("%11d", x);
 
 //na konci pro to, aby se program hned nevypl
 system("pause");
+
+//fukce
+void nazev(int z) //int pro vnější vstup (z se zde deklaruje)
+                  //místo void lze použít například double nebo int
+{
+    //vnitřek funkce
+    return z; //z je výsledek funkce, hodnotu uloží do původní rovnice
+}
+
+//volání funkce
+a = b +nazev(y) //k b se přičte návratová hodnota funkce
+
+//deklarace pole
+int p[20]; //pole p o velikosti 20 znaků
+
+int q[20][30]; //dvourozměrné pole
+
+//práce s poli
+p[5] = x; //na páté místo pole p uloží hodnotu x
+
+//deklarace souboru
+FILE *f1;
+FILE *f2;
+
+//otevření souboru
+f1=fopen("vstup.dat","r"); //otevření souboru f1 (vstup.dat) ke čtení
+f2=fopen("vystup.dat","w"); //otevření souboru f2 (vystup.dat) k zápisu
+
+//čtení souboru
+while (fscanf(f1,"%d",&a)==1)//cyklické čtení hodnoty a ze souboru f1 až do jeho konce
+
+//zápis dat
+fprintf(f2,"%d",a*a); //zápis hodnoty a*a do souboru f2
+
+//uzavření souboru
+fclose(f1); fclose(f2); //uzavření obou souborů f1 a f2
